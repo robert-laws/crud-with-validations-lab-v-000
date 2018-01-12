69606970ad302f08c469a446d3dd61a06d48e82b
Robert Laws <roblaws@me.com>
2018-01-12 17:26:35 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
 
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year"
+  }
 
   validates :released, inclusion: { in: [true, false]}
 
