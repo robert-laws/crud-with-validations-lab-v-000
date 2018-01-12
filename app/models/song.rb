0852914ee343e03363daaec6ca80530fc7f43e48
Robert Laws <roblaws@me.com>
2018-01-12 17:14:07 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  
 
   validates :released, inclusion: { in: [true, false]}
+
+  validates :release_year, presence: true, if: :released == true
 
   validates :artist_name, presence: true
 end
