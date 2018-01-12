4cfa426d6b0e41c53e3c8f650671f0eb1b602f6b
Robert Laws <roblaws@me.com>
2018-01-12 17:25:17 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [true, false]}
 
   validates :release_year, presence: true, if: :released
-  validates :release_year, length: { maximum: 2018 }
+  validates :release_year, length: { maximum: 2020 }
 
   validates :artist_name, presence: true
 end
